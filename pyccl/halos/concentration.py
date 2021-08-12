c14433e8333e50bafc4af73731ab6d67a741cdb0
Nick Koukoufilippas <nikfilippas@gmail.com>
2021-08-12 19:55:11 +0300

    experimental: duffy 500c extrapolation

--- pyccl/halos/concentration.py
+++ pyccl/halos/concentration.py
@@ -367,6 +367,38 @@ class ConcentrationDuffy08(Concentration):
         return self.A * (M * M_pivot_inv)**self.B * a**(-self.C)
 
 
+class ConcentrationDuffy08M500c(Concentration):
+    """ Concentration-mass relation by Duffy et al. 2008
+    (arXiv:0804.2486) extended to Delta = 500-critical.
+    Args:
+        mdef (:class:`~pyccl.halos.massdef.MassDef`): a mass
+            definition object that fixes
+            the mass definition used by this c(M)
+            parametrization.
+    """
+    name = 'Duffy08M500c'
+
+    def __init__(self, mdef=None):
+        super(ConcentrationDuffy08M500c, self).__init__(mdef)
+
+    def _default_mdef(self):
+        self.mdef = MassDef(500, 'critical')
+
+    def _check_mdef(self, mdef):
+        if (mdef.Delta != 500) or (mdef.rho_type != 'critical'):
+            return True
+        return False
+
+    def _setup(self):
+        self.A = 3.67
+        self.B = -0.0903
+        self.C = -0.51
+
+    def _concentration(self, cosmo, M, a):
+        M_pivot_inv = cosmo.cosmo.params.h * 5E-13
+        return self.A * (M * M_pivot_inv)**self.B * a**(-self.C)
+
+
 class ConcentrationIshiyama21(Concentration):
     """ Concentration-mass relation by Ishiyama et al. 2021
     (arXiv:2007.14720). This parametrization is only valid for
